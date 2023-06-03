b843368d29a974dc16dae99ded242678a004d34f
Grzegorz Kowalczyk <gregorzkowalczykk@gmail.com>
2023-06-03 13:02:11 +0200

    Commit 3

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
 
 segment_list = []
 
-def creating_snake():
-    x_position = [0, -20, -40]
-    for segment_index in range(3):
-        new_segment = Turtle(shape="square")
-        new_segment.color("white")
-        new_segment.pu()
-        new_segment.goto(x=x_position[segment_index] , y=0)
-        segment_list.append(segment_index)
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-creating_snake()
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
 
 
 
