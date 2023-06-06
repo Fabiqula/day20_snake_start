c71985e837e4d3d202f586ad9383fa9e2976ee3c
Grzegorz Kowalczyk <gregorzkowalczykk@gmail.com>
2023-06-06 12:05:02 +0200

    Commit 4 - no tail colision

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -8,9 +10,9 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-segment_list = []
-
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,6 +26,20 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+    if snake.head.distance(food) < 10:
+        snake.extend()
+
+        if food.shape() == "circle":
+            scoreboard.increase_score()
+        if food.shape() == "turtle":
+            scoreboard.track_score_turtle()
+
+        food.get_shape()
+        food.refresh()
+
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 260 or snake.head.ycor() < -290:
+        scoreboard.colide()
+        game_is_on = False
 
 
 
--- snake.py
+++ snake.py
@@ -17,11 +17,18 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.pu()
-            new_segment.goto(position)
-            self.segment_list.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.pu()
+        new_segment.goto(position)
+        self.segment_list.append(new_segment)
+
+
+    def extend(self):
+        self.add_segment(self.segment_list[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segment_list) - 1, 0, -1):
